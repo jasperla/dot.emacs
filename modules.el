7114673566395a432b5dcded0472cd8a4cb85461
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2011-12-08 13:00:00 +0100

    Add and hookup paredit.

--- modules.el
+++ modules.el
@@ -25,3 +25,9 @@
 (autoload 'lua-mode "lua-mode")
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-hook 'lua-mode-hook (lambda () (setq lua-indent-level 2)))
+
+;; Paredit and enable it by default for clojure-mode
+(load "~/.emacs.d/modules/paredit")
+(autoload 'enable-paredit-mode "paredit"
+  "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)

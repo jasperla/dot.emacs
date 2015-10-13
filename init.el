6b367a788e3b1fd4c5918a48dad70727679d068a
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2015-10-13 14:50:16 +0200

    run 'custom-set-variables' before loading sml

--- init.el
+++ init.el
@@ -1,15 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
-(require 'packages)
-(require 'modules)
-(require 'utils)
-(require 'keybindings)
-(require 'layout)
-(require 'c-mode-sane)
-(require 'org-mode)
-(when window-system
-  (require 'x11))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,3 +15,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(require 'packages)
+(require 'modules)
+(require 'utils)
+(require 'keybindings)
+(require 'layout)
+(require 'c-mode-sane)
+(require 'org-mode)
+(when window-system
+  (require 'x11))
